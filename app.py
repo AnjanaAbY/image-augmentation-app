69f58f546a61105a6562f3fff7f8ae89cbda7a0b
Anjana Benny <anjanabenny678@gmail.com>
2024-07-19 20:53:39 +0530

    Updated app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
 from flask import Flask, request, render_template, send_file
-from flask_uploads import UploadSet, configure_uploads, IMAGES
 import cv2
 import numpy as np
 import os
 from io import BytesIO
 from zipfile import ZipFile
 import albumentations as A
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# Configure file uploads
-photos = UploadSet('photos', IMAGES)
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
-configure_uploads(app, photos)
+# Define the folder to save uploaded files
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
-# Home route
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Image upload and augmentation route
 @app.route('/upload', methods=['POST'])
 def upload():
     files = request.files.getlist('photos')
     augmented_images = []
 
     for file in files:
-        filename = photos.save(file)
-        filepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
-        augmented_images.extend(augment_image(filepath))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            augmented_images.extend(augment_image(filepath))
 
     # Create a zip file of the augmented images
     zip_buffer = create_zip(augmented_images)
@@ -40,18 +45,18 @@ def augment_image(filepath):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     augmented_images = []
 
-    # Define only the 90 degrees rotation augmentation
+    # Define augmentations
     augmentations = [
         A.Rotate(limit=(90, 90), p=1),  # 90 degrees rotation
-	A.Rotate(limit=(180, 180), p=1),  # 180 degrees rotation
-  	A.Rotate(limit=(270, 270), p=1),  # 270 degrees rotation
-	A.HorizontalFlip(p=1),  # Horizontal flip
-	A.VerticalFlip(p=1),  # Vertical flip
-	A.Compose([
+        A.Rotate(limit=(180, 180), p=1),  # 180 degrees rotation
+        A.Rotate(limit=(270, 270), p=1),  # 270 degrees rotation
+        A.HorizontalFlip(p=1),  # Horizontal flip
+        A.VerticalFlip(p=1),  # Vertical flip
+        A.Compose([
             A.CenterCrop(height=int(image.shape[0]*0.8), width=int(image.shape[1]*0.8), p=1),
             A.Resize(height=image.shape[0], width=image.shape[1], p=1)
         ], p=1),
-	A.RandomBrightnessContrast(p=1)
+        A.RandomBrightnessContrast(p=1)
     ]
 
     # Apply each augmentation independently to the original image
